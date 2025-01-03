8e69ea8706ab8024cbd8d41191be8ad5ed244f58
admin <admin@admins-MacBook-Pro-2.local>
2025-01-02 20:09:47 -0800

    Update codebase and fix render deployment issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,18 @@
+// src/main.jsx
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NBAData from "./components/NBAData";
-import GameDetails from "./components/GameDetails";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        {/* Home Page */}
-        <Route path="/" element={<NBAData />} />
-
-        {/* Game Details Page */}
-        <Route path="/game/:id" element={<GameDetails />} />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
